c68af0ae44af7928a7c38110cc3aa8d532120f3d
Patrick Hoefer <hoefer@sipgate.de>
2021-07-14 17:05:52 +0200

    refactor(rest): add logging

--- src/main/kotlin/com/xetra11/testrestapi/api/TestApi.kt
+++ src/main/kotlin/com/xetra11/testrestapi/api/TestApi.kt
@@ -1,5 +1,7 @@
 package com.xetra11.testrestapi.api
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 class TestApi {
     @GetMapping("/test")
     fun get(): HttpStatus {
+        LOG.info("Request Arrived!")
         return HttpStatus.ACCEPTED
+    }
+
+    companion object {
+        private val LOG: Logger = LoggerFactory.getLogger(TestApi::class.java.name)
     }
 }
